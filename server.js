1b2256893b820ff9b48dc3f5b6dc2b2df7b9adc9
individual-ism <alexlchang@icloud.com>
2022-12-29 12:27:41 -0500

    Update

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import morgan from "morgan"
 import routes from "./routes/index.js"
 
 let application = express()
-let PORT = process.env.PORT || 4150
+let port = process.env.PORT || 4150
 
 application.use(cors())
 application.use(express.json())
@@ -16,7 +16,7 @@ application.use("/", routes)
 database.on("connected", () => {
     console.clear()
     console.log("Connection to MongoDB: Successful")
-    application.listen(PORT, () => {
-        console.log(`Listening on port ${PORT}`)
+    application.listen(port, () => {
+        console.log(`Listening on port ${port}`)
     })
 })
